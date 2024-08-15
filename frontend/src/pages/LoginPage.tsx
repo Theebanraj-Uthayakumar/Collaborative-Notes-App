be6edbc8c828849907c0e324e84ab1d2984f0a93
Theeban <theebanrajuthayakumar10@gmail.com>
2024-08-16 00:35:25 +0530

    add Login and Register style and validations

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,47 +1,154 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { validateEmail } from "../shared/utils/helpers";
+import { toast } from "react-toastify";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<{ email: string; password: string }>({
+    email: "",
+    password: "",
+  });
   const { login } = useAuth();
-
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    try {
-      await login(email, password);
-      navigate("/");
-    } catch (error) {
-      console.error("Login failed:", error);
+    setError({ email: "", password: "" });
+
+    if (!email || !password) {
+      setError({
+        ...error,
+        email: !email ? "Email is required." : "",
+        password: !password ? "Password is required." : "",
+      });
+      return;
+    } else {
+      if (!validateEmail(email)) {
+        setError({ ...error, email: "Please enter a valid email address." });
+        return;
+      }
+
+      if (password.length < 6) {
+        setError({
+          ...error,
+          password: "Password must be at least 6 characters long.",
+        });
+        return;
+      }
+
+      try {
+        await login(email, password);
+        toast.success("Login successful!");
+        navigate("/");
+      } catch (error) {
+        toast.error(
+          "Login failed. Please check your credentials and try again."
+        );
+        console.error("Login failed:", error);
+      }
     }
   };
 
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setError({ ...error, email: "" });
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setError({ ...error, password: "" });
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Email:</label>
+    <form onSubmit={handleSubmit} style={styles.form}>
+      <h2 style={styles.title}>Login</h2>
+      <div style={styles.field}>
+        <label style={styles.label}>Email:</label>
         <input
-          type="email"
+          type="text"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
+          onChange={handleEmailChange}
+          style={styles.input}
         />
+        {error && <div style={styles.error}>{error?.email}</div>}
       </div>
-      <div>
-        <label>Password:</label>
+      <div style={styles.field}>
+        <label style={styles.label}>Password:</label>
         <input
           type="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
+          onChange={handlePasswordChange}
+          style={styles.input}
         />
+        {error && <div style={styles.error}>{error?.password}</div>}
       </div>
-      <button type="submit">Login</button>
+      <button type="submit" style={styles.button}>
+        Login
+      </button>
+      <div style={styles.signup}>
+        Don't have an account?{" "}
+        <Link to="/signup" style={styles.signupLink}>
+          Sign up here
+        </Link>
+      </div>
     </form>
   );
+};
+
+const styles = {
+  form: {
+    maxWidth: "400px",
+    margin: "0 auto",
+    padding: "20px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    backgroundColor: "#f9f9f9",
+  },
+  title: {
+    textAlign: "center" as const,
+    marginBottom: "20px",
+  },
+  field: {
+    marginBottom: "15px",
+  },
+  label: {
+    display: "block",
+    marginBottom: "5px",
+    fontWeight: "bold",
+  },
+  input: {
+    width: "-webkit-fill-available",
+    padding: "8px",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+  },
+  button: {
+    width: "100%",
+    padding: "10px",
+    backgroundColor: "#4CAF50",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "16px",
+  },
+  error: {
+    color: "red",
+    padding: "5px",
+    marginBottom: "12px",
+    fontSize: "14px",
+  },
+  signup: {
+    marginTop: "15px",
+    textAlign: "center" as const,
+  },
+  signupLink: {
+    color: "#4CAF50",
+    textDecoration: "none",
+  },
 };
 
 export default Login;
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,55 +1,214 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
+import { validateEmail } from "../shared/utils/helpers";
+import { toast } from "react-toastify";
+import { useNavigate, Link } from "react-router-dom";
 
 const Register: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<{
+    username: string;
+    password: string;
+    confirmPassword: string;
+    email: string;
+  }>({
+    username: "",
+    password: "",
+    confirmPassword: "",
+    email: "",
+  });
   const { register } = useAuth();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    try {
-      await register(username, password, email);
-      // Redirect to the login page or dashboard after registration
-    } catch (error) {
-      console.error("Registration failed:", error);
-      // Handle registration errors (e.g., show an error message)
+    setError({ username: "", password: "", confirmPassword: "", email: "" });
+
+    if (!email || !username || !password || !confirmPassword) {
+      setError({
+        ...error,
+        email: !email ? "Email is required." : "",
+        username: !username ? "Username is required." : "",
+        password: !password ? "Password is required." : "",
+        confirmPassword: !confirmPassword
+          ? "Confirm Password is required."
+          : "",
+      });
+      return;
+    } else {
+      if (!validateEmail(email)) {
+        setError({ ...error, email: "Please enter a valid email address." });
+
+        return;
+      }
+
+      if (password.length < 6) {
+        setError({
+          ...error,
+          password: "Password must be at least 6 characters long.",
+        });
+
+        return;
+      }
+
+      if (password !== confirmPassword) {
+        setError({
+          ...error,
+          confirmPassword: "Passwords do not match.",
+        });
+
+        return;
+      }
+
+      try {
+        await register(username, password, email);
+        toast.success("Registration successful! You can now log in.");
+        navigate("/");
+      } catch (error) {
+        toast.error("Registration failed. Please try again.");
+        console.error("Registration failed:", error);
+      }
     }
   };
 
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setError({ ...error, email: "" });
+  };
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+    setError({ ...error, username: "" });
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setError({ ...error, password: "" });
+  };
+
+  const handleConfirmPasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setConfirmPassword(e.target.value);
+    setError({ ...error, confirmPassword: "" });
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Email:</label>
+    <form onSubmit={handleSubmit} style={styles.form}>
+      <h2 style={styles.title}>Register</h2>
+      <div style={styles.field}>
+        <label style={styles.label}>Email:</label>
         <input
           type="email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
+          onChange={handleEmailChange}
+          style={styles.input}
         />
+        {error && <div style={styles.error}>{error?.email}</div>}
       </div>
-      <button type="submit">Register</button>
+      <div style={styles.field}>
+        <label style={styles.label}>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+          style={styles.input}
+        />
+        {error && <div style={styles.error}>{error?.username}</div>}
+      </div>
+      <div style={styles.field}>
+        <label style={styles.label}>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          style={styles.input}
+        />
+        {error && <div style={styles.error}>{error?.password}</div>}
+      </div>
+      <div style={styles.field}>
+        <label style={styles.label}>Confirm Password:</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+          style={styles.input}
+        />
+        {error && <div style={styles.error}>{error?.confirmPassword}</div>}
+      </div>
+      <button type="submit" style={styles.button}>
+        Register
+      </button>
+      <div style={styles.signin}>
+        Already have an account?{" "}
+        <Link to="/signin" style={styles.signinLink}>
+          Sign In here
+        </Link>
+      </div>
     </form>
   );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  form: {
+    maxWidth: "400px",
+    margin: "0 auto",
+    padding: "20px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    backgroundColor: "#f9f9f9",
+  },
+  title: {
+    textAlign: "center" as const,
+    marginBottom: "20px",
+  },
+  field: {
+    marginBottom: "15px",
+  },
+  label: {
+    display: "block",
+    marginBottom: "5px",
+    fontWeight: "bold",
+  },
+  input: {
+    width: "-webkit-fill-available",
+    padding: "8px",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+  },
+  button: {
+    width: "100%",
+    padding: "10px",
+    backgroundColor: "#4CAF50",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "16px",
+  },
+  error: {
+    color: "red",
+    padding: "5px",
+    marginBottom: "12px",
+    fontSize: "14px",
+  },
+  signin: {
+    marginTop: "15px",
+    textAlign: "center" as const,
+  },
+  signinLink: {
+    color: "#4CAF50",
+    textDecoration: "none",
+  },
 };
 
 export default Register;
