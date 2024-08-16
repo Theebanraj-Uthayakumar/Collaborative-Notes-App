683308a0c32c88b0019a12fe388edabf4ae6696f
Theeban <theebanrajuthayakumar10@gmail.com>
2024-08-16 21:19:02 +0530

    refactor notes.service.ts to use CommonService for HTTP calls

---
+++ frontend/src/service/notes.service.ts
@@ -0,0 +1,37 @@
+import ApiConstants from "../shared/constants/apiConstants";
+import { Notes } from "../shared/interfaces";
+import CommonService from "./common.service";
+
+export const getNotes = async () => {
+  const response = await CommonService?.invokeHttpCallFetch(
+    "GET",
+    ApiConstants?.GET_NOTES_API
+  );
+  return response;
+};
+
+export const createNote = async (note: Notes) => {
+  const response = await CommonService?.invokeHttpCallFetch(
+    "POST",
+    ApiConstants?.CREATE_NOTE_API,
+    note
+  );
+  return response;
+};
+
+export const updateNote = async (id: string, note: Notes) => {
+  const response = await CommonService?.invokeHttpCallFetch(
+    "PUT",
+    `${ApiConstants?.UPDATE_NOTE_API}/${id}`,
+    note
+  );
+  return response;
+};
+
+export const deleteNote = async (id: string) => {
+  const response = await CommonService?.invokeHttpCallFetch(
+    "DELETE",
+    `${ApiConstants?.DELETE_NOTE_API}/${id}`
+  );
+  return response;
+};
