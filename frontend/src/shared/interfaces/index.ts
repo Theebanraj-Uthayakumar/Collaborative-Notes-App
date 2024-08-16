22704d2118647df1e35d983c8d81406a8a80319b
Theeban <theebanrajuthayakumar10@gmail.com>
2024-08-16 21:17:38 +0530

    Add interfaces for UserDetails, Notes, and NotesList

---
+++ frontend/src/shared/interfaces/index.ts
@@ -0,0 +1,16 @@
+export interface UserDetails {
+  id: string;
+  username: string;
+  email: string;
+}
+
+export interface Notes {
+  title: string;
+  content: string;
+}
+
+export interface NotesList {
+    _id: string;
+    title: string;
+    content: string;
+}
